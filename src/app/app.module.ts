bd35adeff42eb43e040c6128be9ebeaeb1c1ff5d
Nilson Junior da Silva <nilson.silva@verreschi.com>
2018-10-08 11:58:06 -0300

    injeção de dependencia e HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FotosModule } from './fotos/fotos.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FotosModule } from './fotos/fotos.module';
   ],
   imports: [
     BrowserModule,
-    FotosModule
+    FotosModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
